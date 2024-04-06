70f9b0dcd22a5d2770c58bdee34751fc29b4131f
stfmdc <mandicstefan94@gmail.com>
2024-04-06 15:19:53 +0200

    update

---
+++ Homeworks/Class03/StudentGroup/StudentGroup/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.ComponentModel.Design;
+
+namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsG2 = { "Stefan", "Marko", "Janko", "Petar", "Goran" };
+
+            int userInput;
+            do
+            {
+                Console.Write("Molimo unestite broj grupe studenata čija imena želite da vidite na ekranu (trenutno dostupni brojevi 1 i 2): ");
+            } while (!int.TryParse(Console.ReadLine(), out userInput) || (userInput != 1 && userInput != 2));
+
+            if (userInput == 1)
+            {
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine($"Lista studenata iz Grupe 1 je:");
+                Console.WriteLine("--------------------------------------");
+                foreach (string student in studentsG1)
+                {
+                    Console.WriteLine(student);
+                }
+                Console.WriteLine("--------------------------------------");
+            }
+            else
+            {
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine($"Lista studenata iz Grupe 2 je:");
+                Console.WriteLine("--------------------------------------");
+                foreach (string student in studentsG2)
+                {
+                    Console.WriteLine(student);
+                }
+                Console.WriteLine("--------------------------------------");
+            }
+
+        }
+    }
+}
---
+++ Homeworks/Class03/SumOfEven/SumOfEven/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SumOfEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = new int[6];
+            Console.WriteLine("Unesite 6 brojeva:");
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                string userInput = Console.ReadLine();
+                intArray[i] = int.Parse(userInput);
+            }
+
+            int sum = 0;
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (intArray[i] % 2 == 0)
+                {
+                    sum += intArray[i];
+                }
+            }
+
+            Console.WriteLine($"Zbir parnih brojeva koje ste uneli je: {sum}");
+        }
+    }
+}
